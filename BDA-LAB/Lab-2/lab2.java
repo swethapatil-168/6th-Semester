b740d569fc1634cdd515b2236fd11d2048d99daf
swethapatil-168 <71492810+swethapatil-168@users.noreply.github.com>
2022-06-07 22:46:18 +0530

    Create lab2.java

---
+++ BDA-LAB/Lab-2/lab2.java
@@ -0,0 +1,30 @@
+   
+import java.sql.*;
+
+
+public class lab1 {
+
+
+	public static void main(String[] args)
+	{
+
+		Connection con = null;
+		PreparedStatement ps = null;
+    
+		con = connection.connectDB();
+
+		try {
+
+			String sql = "insert into empdetails values('Swetha Patil','SP','abc.cs19@bmsce.ac.in','SDE','5454878546',10)";
+			ps = con.prepareStatement(sql);
+
+			ps.execute();
+		}
+
+
+		catch (Exception e) {
+
+			System.out.println(e);
+		}
+	}
+}
